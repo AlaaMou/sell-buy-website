d69910594c12b1494e92f70be9c2f694faac8ba3
Alaa <alaa.moussa2018@gmail.com>
2019-04-07 23:20:30 +0000

    edit dotenv

--- controllers/post.js
+++ controllers/post.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+// require('dotenv').config()
 const Post       = require("../models/post");
 const Review     = require("../models/review");
 const {cloudinary} = require("../cloudinary");
